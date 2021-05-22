f3de896251e17024996b8278179fd1b96c0cc988
Kristina Lesnjakovic <55280843+kristinalesnjakovic@users.noreply.github.com>
2021-05-22 15:41:27 +0200

    Add files via upload
    Fixed event listener

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var button = document.querySelector(".share-cta");
 
 button.addEventListener("click", function(e) {
-    e.target.parentElement.parentElement.parentElement.parentElement.classList.toggle("active");
+  document.querySelector(".container-card").classList.toggle("active");
   });
-
 
